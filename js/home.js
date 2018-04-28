72785f0c403152e94666b7282f96f1e00c6e758d
diep <diepbui@gmail.com>
2018-04-28 12:30:08 +0700

    commit 13

--- js/home.js
+++ js/home.js
@@ -5,4 +5,33 @@ $(function(){
 		var docao = $(window).height();
 		$('.img-carousel').css({'height':docao});
 	});
+
+	//ham click
+	$(".logo").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#myCarousel").offset().top - 100
+	    }, 1500);
+	    return false;
+	});
+
+	$(".n1").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#about").offset().top - 55
+	    }, 1500);
+	    return false;
+	});
+
+	$(".n2").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#portfolio").offset().top - 55
+	    }, 1500);
+	    return false;
+	});
+
+	$(".n3").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#contact").offset().top - 55
+	    }, 1500);
+	    return false;
+	});
 });
